9992b5df175ad0f4a8cabca30a50341d75edd7df
Muhammad Rifat <hrifat450@gmail.com>
2021-02-23 22:00:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const personName = [
+    {name:'Rifat', job:'Software Engineer'},
+    {name:'Romjan', job:'Executive Magistrate'}
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person person={personName[0]}></Person>
+        <Person person={personName[1]}></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props){
+  console.log(props);
+  return(
+    <div style={{border:"1px solid white", margin:"10px", padding:"10px"}}>
+      <h2>Name: {props.person.name}</h2>
+      <p>{props.person.job}</p>
     </div>
   );
 }
